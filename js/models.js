2bd4367389738ac8409d5df012cabb0b6cdafcb8
batman <batman@batman.com>
2022-06-07 19:25:46 -0300

    SiteSenai

---
+++ js/models.js
@@ -0,0 +1,29 @@
+/*
+=========================================================================
+Nome do Projeto: Carrinho de Compras em Javascript
+Finalidade: 1DS 
+Autor: Astarote
+
+========================================================================= 
+*/
+
+let modelsJson = [
+    {id:1, name:'Alface', img:'img/alface.png', price:[4.79, 9.58, 14.37], sizes:['1', '2', '3'], description:'Alface Crespa Unidade'},
+    {id:2, name:'Cenoura', img:'img/cenoura.png', price:[1.80, 3.60, 5.4], sizes:['1', '2', '3'], description:'Cenoura 1 Unidade 200g'},
+    {id:3, name:'Pepino', img:'img/pepino.png', price:[3.00, 6.00, 9.00], sizes:['1', '2', '3'], description:'Pepino Caipira 1 Unidade 350g'},
+    {id:4, name:'Maçã', img:'img/maca.png', price:[2.30, 6.60, 8.90], sizes:['1', '2', '3'], description:'Maçã Fuji 1 Unidade 200g'},
+    {id:5, name:'Banana', img:'img/banana.png', price:[8.00, 16.00, 32.00], sizes:['1', '2', '3'], description:'Banana Nanica 1 Cacho 1.500kg'},
+    {id:6, name:'Uva', img:'img/uva.png', price:[10.00, 20.00, 30.00], sizes:['1', '2', '3'], description:'Uva Thompson Cumbuca Unidade'},
+    {id:7, name:'Cerveja', img:'img/cerveja.png', price:[2.39, 4.78, 7.17], sizes:['1', '2', '3'], description:'Cerveja Brahma unidade lata'},
+    {id:8, name:'Energético', img:'img/monster.png', price:[10.00, 20.00, 30.00], sizes:['1', '2', '3'], description:'Energético Monster diversos sabores'},
+    {id:9, name:'Coca-Cola', img:'img/coca.png', price:[8.00, 16.00, 24.00], sizes:['1', '2', '3'], description:'Coca-Cola 1,5 litros'},
+    {id:10, name:'Filé de Frango', img:'img/frango.png', price:[8.99, 17.99, 26.98], sizes:['1', '2', '3'], description:'Bandeja de filé de frango'},
+    {id:11, name:'Carne', img:'img/carne.png', price:[12.00, 24.00, 36.00], sizes:['1', '2', '3'], description:'Bandeja de Alcatra'},
+    {id:12, name:'Salsinha', img:'img/salsicha.png', price:[9.99, 19,98, 29,97], sizes:['1', '2', '3'], description:'Bandeja de Salsinha Sadia'},
+    {id:13, name:'Pasta dental', img:'img/pasta.png', price:[3.00, 6.00, 9.00], sizes:['1', '2', '3'], description:'Pasta de dente colgate 1 unidade'},
+    {id:14, name:'Sabonetes', img:'img/sabonete.png', price:[1.25, 2.50, 2.75], sizes:['1', '2', '3'], description:'Sabonetes diversas marcas 1 unidade'},
+    {id:15, name:'Shampoo', img:'img/shampoo.png', price:[26.90, 20.00, 30.00], sizes:['1', '2', '3'], description:'Shampoo e condicionador Dove'},
+    {id:16, name:'Detergente', img:'img/detergente.png', price:[3.22, 6.44, 8.66], sizes:['1', '2', '3'], description:'Detergente Ypê 1 unidade'},
+    {id:17, name:'Água sanitária', img:'img/aguaSanitaria.png', price:[9.00, 18.00, 27.00], sizes:['1', '2', '3'], description:'Água sanitária diversas marcas'},
+    {id:18, name:'Sabão', img:'img/sabao.png', price:[7.99, 15.98, 23.97], sizes:['1', '2', '3'], description:'Energético Monster diversos sabores'}
+];
